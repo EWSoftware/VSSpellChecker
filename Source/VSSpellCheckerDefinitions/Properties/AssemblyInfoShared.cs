b909810eb662d23728b1dc90b86c46d26e022f10
EWSoftware <Eric@EWoodruff.us>
2019-07-08 10:47:54 -0700

    Updated version number for release

--- Source/VSSpellCheckerDefinitions/Properties/AssemblyInfoShared.cs
+++ Source/VSSpellCheckerDefinitions/Properties/AssemblyInfoShared.cs
@@ -2,7 +2,7 @@
 // System  : Visual Studio Spell Checker
 // File    : AssemblyInfoShared.cs
 // Author  : Eric Woodruff  (Eric@EWoodruff.us)
-// Updated : 05/05/2019
+// Updated : 07/08/2019
 // Note    : Copyright 2013-2019, Eric Woodruff, All rights reserved
 // Compiler: Microsoft Visual C#
 //
@@ -83,13 +83,13 @@ internal static partial class AssemblyInfo
     //
     // This is used to set the assembly file version.  This will change with each new release.  MSIs only
     // support a Major value between 0 and 255 so we drop the century from the year on this one.
-    public const string FileVersion = "19.5.5.0";
+    public const string FileVersion = "19.7.8.0";
 
     // Common product version
     //
     // This may contain additional text to indicate Alpha or Beta states.  The version number will always match
     // the file version above but includes the century on the year.
-    public const string ProductVersion = "2019.5.5.0";
+    public const string ProductVersion = "2019.7.8.0";
 
     // Assembly copyright information
     public const string Copyright = "Copyright \xA9 2013-2019, Eric Woodruff, All Rights Reserved.\r\n" +
